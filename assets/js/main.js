ae3f23143c0d3c0a40a15bf86377133b6400295a
Raphael Amorim <rapha850@gmail.com>
2014-03-29 16:41:03 -0300

    little adjust (3)

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,15 +23,13 @@ $(function() {
  			if($("#bar-mobile-menu").css("opacity") <= 0) {
          $("#bar-mobile-menu").css('height', '415px');
  				$("#bar-mobile-menu").animate({
-             opacity: 1,
-             paddingLeft: '0px'
-         }, 100);
+             opacity: 1
+         }, 0);
  			}
  			else {
  				$("#bar-mobile-menu").animate({
-           opacity: 0,
-           paddingLeft: '1000px'
-         }, 100);
+           opacity: 0
+         }, 0);
          $("#bar-mobile-menu").css('height', '0px');
  			}
  		});
