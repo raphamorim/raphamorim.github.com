5f95b1a9a2c68124aff57748e84941eaf9e009cc
Raphael Amorim <rapha850@gmail.com>
2014-04-30 18:01:51 -0300

    improve js function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,32 @@
-$(document).ready(function() {
+var url = window.location.pathname,
+    urlRegExp = new RegExp(url == '/' ? window.location.origin + '/?$' : url.replace(/\/$/, ''));
 
-    var url = window.location.pathname,
-        urlRegExp = new RegExp(url == '/' ? window.location.origin + '/?$' : url.replace(/\/$/, ''));
+$('#pagenav a')
+    .each(function() {
+        if (urlRegExp.test(this.href.replace(/\/$/, ''))) {
+            $(this).addClass('active');
+        }
+    });
 
-    $('#pagenav a')
-        .each(function() {
-            if (urlRegExp.test(this.href.replace(/\/$/, ''))) {
-                $(this).addClass('active');
-            }
-        });
+$('#bar-mobile-menu a')
+    .each(function() {
+        if (urlRegExp.test(this.href.replace(/\/$/, ''))) {
+            $(this).addClass('active');
+        }
+    });
 
-    $('#bar-mobile-menu a')
-        .each(function() {
-            if (urlRegExp.test(this.href.replace(/\/$/, ''))) {
-                $(this).addClass('active');
-            }
-        });
 
-    $('.bar-menu-optional')
-        .click(function() {
-            if ($("#bar-mobile-menu").css("opacity") <= 0) {
-                $("#bar-mobile-menu").css('height', '415px');
-                $("#bar-mobile-menu").animate({
-                    opacity: 1
-                }, 0);
-            } else {
-                $("#bar-mobile-menu").animate({
-                    opacity: 0
-                }, 0);
-                $("#bar-mobile-menu").css('height', '0px');
-            }
-        });
-});
+$('.bar-menu-optional')
+    .click(function() {
+        if ($("#bar-mobile-menu").css("opacity") <= 0) {
+            $("#bar-mobile-menu").css('height', '415px');
+            $("#bar-mobile-menu").animate({
+                opacity: 1
+            }, 0);
+        } else {
+            $("#bar-mobile-menu").animate({
+                opacity: 0
+            }, 0);
+            $("#bar-mobile-menu").css('height', '0px');
+        }
+    });
