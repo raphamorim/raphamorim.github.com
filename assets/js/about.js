d7e2f7bdbb7ae8b3042298e26da24931efc3333e
Raphael Amorim <rapha850@gmail.com>
2014-07-17 10:00:09 -0300

    make about js

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     var loading = setInterval(function() {
         if (document.readyState === "complete") {
-            
+            $(".welcome").text("Raphael Amorim");
+            $(".intro").fadeIn(1500);
 
             clearInterval(loading);
         }
